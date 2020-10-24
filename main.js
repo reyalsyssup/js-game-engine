6444d801306f42c74e318737d38e9e4a9327b636
Kai <m3sv.is.me@gmail.com>
2020-10-24 16:04:13 +1100

    image loading and drawing demo

--- main.js
+++ main.js
@@ -1,10 +1,14 @@
 let canvas = document.querySelector("canvas");
 let scene = new Scene(canvas, 500, 500);
 
+let person = new Sprite(scene, "testImage.png", 0, 0);;
+
 function setup() {
     scene.bgColor = "rgb(184, 184, 184)";
 }
 
 function draw() {
     scene.clear(false);
+    person.draw();
+    person.x += 1;
 }
