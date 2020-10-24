309dd6c62d56230ec7b455830dc1ab1316556342
Kai <m3sv.is.me@gmail.com>
2020-10-24 18:32:53 +1100

    can press more than 1 key once

--- keyboard.js
+++ keyboard.js
@@ -1,19 +1,32 @@
 let keyDown;
 let requested = false;
+let keyLog = [];
 
 window.addEventListener("keydown", event => {
     keyDown = event.key;
+    // Prevents duplication of keys in keylog
+    let counter = 0;
+    let alreadyIn = false;
+    console.log("before", keyLog);
+    for(let i = 0; i < keyLog.length; i++) {
+        if(keyLog[i] === keyDown) counter++;
+        if(counter > 0) {
+            alreadyIn = true;
+            break;
+        }
+    }
+    console.log("after", keyLog);
+    if(!alreadyIn) keyLog.push(keyDown);
 });
 
-window.addEventListener("keyup", () => {
-    keyDown = undefined;
+window.addEventListener("keyup", event => {
+    for(let i = 0; i < keyLog.length; i++) {
+        if(keyLog[i] === event.key) {
+            keyLog.splice(i, 1);
+        }
+    }
 });
 
 function getKeyDown() {
-    requested = true;
-    return tick();
-}
-
-function tick() {
-    if(requested) return keyDown;
+    return keyLog;
 }
