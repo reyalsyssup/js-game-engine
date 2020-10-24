c442c787160857277c91052930a494a70d3921ac
Kai <m3sv.is.me@gmail.com>
2020-10-24 16:48:00 +1100

    fixed semicolons

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 let canvas = document.querySelector("canvas");
 let scene = new Scene(canvas, 500, 500);
 
-let person = new Sprite(scene, "testImage.png", 0, 0);;
+let person = new Sprite(scene, "testImage.png", 0, 0);
 
 function setup() {
     scene.bgColor = "rgb(184, 184, 184)";
@@ -11,9 +11,9 @@ function draw() {
     scene.clear(false);
     person.draw();
 
-    let keyDown = getKeyDown()
-    if(keyDown === "d") person.x += 2
-    if(keyDown === "a") person.x -= 2
-    if(keyDown === "w") person.y -= 2
-    if(keyDown === "s") person.y += 2
+    let keyDown = getKeyDown();
+    if(keyDown === "d") person.x += 2;
+    if(keyDown === "a") person.x -= 2;
+    if(keyDown === "w") person.y -= 2;
+    if(keyDown === "s") person.y += 2;
 }
