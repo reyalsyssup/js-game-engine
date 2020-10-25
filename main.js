7767e3c43218156e28aa9a348d705414272e4b9b
Kai <m3sv.is.me@gmail.com>
2020-10-25 16:13:33 +1100

    example using mouse pos

--- main.js
+++ main.js
@@ -1,9 +1,6 @@
 let canvas = document.querySelector("canvas");
 let scene = new Scene(canvas, 500, 500);
 
-let person = new Sprite(scene, "testImage.png", 20, 20, "rect");
-let block = new Sprite(scene, "block.png", 200, 20, "rect");
-
 let circle1 = new Sprite(scene, "circle1.png", 20, 300, "circle");
 let circle2 = new Sprite(scene, "circle2.png", 200, 300, "circle");
 
@@ -13,19 +10,18 @@ function setup() {
 
 function draw() {
     scene.clear(false);
-    person.draw();
-    block.draw();
     circle1.draw();
     circle2.draw();
 
     let keys = getKeyDown();
-    console.log("rects", Sprite.checkCollide(person, block));
-    console.log("circles", Sprite.checkCollide(circle1, circle2));
+    circle2.x = mouse.x;
+    circle2.y = mouse.y;
+    // console.log("circles", Sprite.checkCollide(circle1, circle2));
     for(let key of keys) {
-        if(key === "d") {circle1.x += 2; person.x += 2}
-        if(key === "a") {circle1.x -= 2; person.x -= 2}
-        if(key === "w") {circle1.y -= 2; person.y -= 2}
-        if(key === "s") {circle1.y += 2; person.y += 2}
+        if(key === "d") circle1.x += 2;
+        if(key === "a") circle1.x -= 2;
+        if(key === "w") circle1.y -= 2;
+        if(key === "s") circle1.y += 2;
     }
 }
 
