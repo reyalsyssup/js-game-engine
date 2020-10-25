d9a17eb0f2277f7b3c167de7785e62765ee2c32f
Kai <m3sv.is.me@gmail.com>
2020-10-25 15:44:01 +1100

    Added rect and circle collision and a type var in constructor and a this.radius for "circle" type.

--- sprite.js
+++ sprite.js
@@ -1,13 +1,34 @@
 class Sprite {
-    constructor(_scene, _path, _x, _y) {
+    constructor(_scene, _path, _x, _y, _type) {
         this.path = "./res/" + _path;
         let img = new Image();
-        img.src = this.path;
-        this.img = img;
+        if (_path.length > 0) {
+            img.src = this.path;
+            this.img = img;
+        }
 
+        this.width = this.img.width;
+        this.height = this.img.height;
+        if(_type === "circle")
+            this.radius = this.width/2;
         this.x = _x;
         this.y = _y;
+        this.type = _type;
         this.ctx = _scene.getContext();
+    }
+
+    // This will assume the is either a sprite, or a rect
+    static checkCollide(obj1, obj2) {
+        if(obj1.type === "rect" && obj2.type === "rect") {
+            if((obj1.x + obj1.width > obj2.x && obj1.x < obj2.x + obj2.width)
+            && (obj1.y + obj1.height > obj2.y && obj1.y < obj2.y + obj2.height)) return true;
+        } else if(obj1.type === "circle" && obj2.type == "circle") {
+            // A circles pos is in the center of the circle
+            let run = obj2.x - obj1.x;
+            let rise = obj2.y - obj1.y;
+            if(Math.sqrt(run * run + rise * rise) <= obj1.radius + obj2.radius) return true;
+        }
+        return false;
     }
 
     draw() {
