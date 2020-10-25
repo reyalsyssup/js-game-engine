9797e10d3882b3b87cbce34e6d881bd5b29964bb
Kai <m3sv.is.me@gmail.com>
2020-10-25 16:25:17 +1100

    fixed a misleading comment

--- sprite.js
+++ sprite.js
@@ -23,7 +23,7 @@ class Sprite {
             if((obj1.x + obj1.width > obj2.x && obj1.x < obj2.x + obj2.width)
             && (obj1.y + obj1.height > obj2.y && obj1.y < obj2.y + obj2.height)) return true;
         } else if(obj1.type === "circle" && obj2.type == "circle") {
-            // A circles pos is in the center of the circle
+            // For this calc, the circle's position is assumed to be the centre. The circles pos is at the top left but the calculation will still work
             let run = obj2.x - obj1.x;
             let rise = obj2.y - obj1.y;
             if(Math.sqrt(run * run + rise * rise) <= obj1.radius + obj2.radius) return true;
